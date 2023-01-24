84772bf43f5113ee6167c1a7e49406d9942d0fa7
markquest <oguzbeliren@hotmail.com>
2023-01-24 16:22:57 -0500

    The Roman Numerial Converter App run in Ec2 CloudFormation fully automated and managed by AWS

--- Project-001-Roman-Numerals-Converter/CLI_solution/userdata.sh
+++ Project-001-Roman-Numerals-Converter/CLI_solution/userdata.sh
@@ -12,3 +12,20 @@ wget ${FOLDER}/templates/index.html
 wget ${FOLDER}/templates/result.html 
 cd ..
 python3 app.py
+
+
+
+
+
+#PROJECT SUMMARY
+# 1) We wrote Roman Numerial Converter App on VsCode
+# 2) We push it to GitHub 
+# 3) We wrote CloudFormation and Template
+# 4) We create an Ec2 Instance and connect to out terminal 
+# 5) We wrote the required code that is located in userdata.sh to our terminal
+#     in order to pull the app from GitHub and make it work in the Ec2 instance that we created. 
+# 6) We also created CloudFormaation Stuck and create a stack, then make our EC2 Instance run it automatically.
+
+#Result
+# We can pull the app from git and make it run in Ec2 CloudFormation fully automated and managed by AWS.
+
