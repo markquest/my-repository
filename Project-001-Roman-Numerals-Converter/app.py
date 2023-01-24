208527ea1d46d1cacedda54fb7b7097640eb7b5c
markquest <oguzbeliren@hotmail.com>
2023-01-24 13:14:08 -0500

    first iteration of code

--- Project-001-Roman-Numerals-Converter/app.py
+++ Project-001-Roman-Numerals-Converter/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 def convert(decimal_num):
     roman = {1000:'M', 900:'CM', 500:'D', 400:'CD', 100:'C', 90:'XC', 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I'}
@@ -26,5 +27,6 @@ def main_post():
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=80)   
+    app.run(debug=True)
+    # app.run(host='0.0.0.0', port=80)   
+    
