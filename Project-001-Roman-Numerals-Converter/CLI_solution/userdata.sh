351333669a4636be9900b9c9a4e41147d8b5b4a9
markquest <oguzbeliren@hotmail.com>
2023-01-24 13:52:23 -0500

    openning port 80 and fixing the ip address for EC2

--- Project-001-Roman-Numerals-Converter/CLI_solution/userdata.sh
+++ Project-001-Roman-Numerals-Converter/CLI_solution/userdata.sh
@@ -4,7 +4,15 @@ yum install python3 -y
 pip3 install flask
 yum install git -y
 cd /home/ec2-user
-wget -P templates https://raw.githubusercontent.com/serdarcw/cli_deneme/master/templates/index.html
-wget -P templates https://raw.githubusercontent.com/serdarcw/cli_deneme/master/templates/result.html
-wget https://raw.githubusercontent.com/serdarcw/cli_deneme/master/app.py
+FOLDER="https://raw.githubusercontent.com/markquest/my-repository/master/Project-001-Roman-Numerals-Converter"
+wget ${FOLDER}/app.py
+mkdir templates
+cd templates
+wget ${FOLDER}/templates/index.html
+wget ${FOLDER}/templates/result.html 
+cd ..
 python3 app.py
+
+https://raw.githubusercontent.com/markquest/my-repository/master/Project-001-Roman-Numerals-Converter/app.py
+https://raw.githubusercontent.com/markquest/my-repository/master/Project-001-Roman-Numerals-Converter/templates/index.html
+https://raw.githubusercontent.com/markquest/my-repository/master/Project-001-Roman-Numerals-Converter/templates/result.html
